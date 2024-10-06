cff0cb1d84e9813cae74a8672923a6c461b8af0e
dani-cavalcanti <danicacoelho@gmail.com>
2024-10-05 23:01:55 -0300

    Prompts inseridos na página Quick Command

--- script.js
+++ script.js
@@ -39,36 +39,30 @@ fetch("menu.html")
   .catch((error) => console.error("Erro ao carregar o menu:", error));
 
 // Função para copiar código
-function applyCopyButtonFunctionality() {
-  document.querySelectorAll(".copy-button").forEach((button) => {
-    button.addEventListener("click", function () {
-      const codeBlock = document.querySelector(
-        this.getAttribute("data-clipboard-target")
-      );
-      const codeText = codeBlock.textContent;
-      navigator.clipboard
-        .writeText(codeText)
-        .then(() => {
-          alert("Código copiado com sucesso!");
-        })
-        .catch((err) => {
-          console.error("Erro ao copiar o código:", err);
-        });
-    });
+// Seleciona todos os elementos com a classe 'copyable'
+const copyableElements = document.querySelectorAll(".copyable");
+
+// Adiciona o evento de clique a cada elemento
+copyableElements.forEach(function (element) {
+  element.addEventListener("click", function () {
+    // Seleciona o texto dentro do <code>
+    const text = this.innerText;
+
+    // Copia o texto para a área de transferência
+    navigator.clipboard
+      .writeText(text)
+      .then(function () {
+        // Exibe o popup de confirmação
+        const popup = document.getElementById("popup");
+        popup.classList.add("show");
+
+        // Remove o popup após 3 segundos
+        setTimeout(function () {
+          popup.classList.remove("show");
+        }, 3000);
+      })
+      .catch(function (err) {
+        console.error("Erro ao copiar o texto: ", err);
+      });
   });
-}
-
-// Chama a função para aplicar a funcionalidade de copiar código
-applyCopyButtonFunctionality();
-
-// Função para alternar entre tema claro e escuro
-const toggleThemeButton = document.getElementById("toggle-theme");
-const currentTheme = localStorage.getItem("theme") || "light";
-document.body.classList.add(currentTheme);
-
-toggleThemeButton.addEventListener("click", () => {
-  const newTheme = document.body.classList.contains("light") ? "dark" : "light";
-  document.body.classList.remove("light", "dark");
-  document.body.classList.add(newTheme);
-  localStorage.setItem("theme", newTheme);
 });
